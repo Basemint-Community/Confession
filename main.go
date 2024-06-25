0eeba3d9aea3a3667ba400c7cef29278663abfab
Shubham <wilfred.shubham@gmail.com>
2024-06-25 12:17:55 +0530

    added actual base and env load

--- main.go
+++ main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 
+	"github.com/Basemint-Community/Confession/Confessions"
 	"github.com/bwmarrin/discordgo"
 )
 
-var (
-	Token = os.Getenv("TOKEN")
-	ConfessionsToken = flag.String("Bot", "", Token)
-	TestGuild = flag.String("guild", "", "1233196678530728008")
-	TestChannel = flag.String("channel", "", "1233196679008616490")
-)
-
 func init() {
-	flag.Parse()
+	Confessions.LoadEnv()
 }
 
 func main() {
-	session, _ := discordgo.New(Token)
-	session.Identify.Intents = discordgo.IntentMessageContent
+	token := os.Getenv("TOKEN")
+
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press Ctrl+C to exit.")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID || m.Author.Bot {
+		return
+	}
+
+	if strings.HasPrefix(m.Content, "!hello") {
+		s.ChannelMessageSend(m.ChannelID, "Hello, I'm a Discord bot!")
+	}
 }
