aa15b35442b1b464f89de589de3185ab2a9a5929
Shubham <wilfred.shubham@gmail.com>
2024-06-25 12:46:09 +0530

    pushed confession command

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.3
 require github.com/bwmarrin/discordgo v0.28.1
 
 require (
+	github.com/Necroforger/dgrouter v0.0.0-20200517224846-e66453b957c1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func init() {
 
 func main() {
 	token := os.Getenv("TOKEN")
+	applicationID := os.Getenv("ApplicationID")
 
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
@@ -25,10 +26,18 @@ func main() {
 	}
 
 	dg.AddHandler(messageCreate)
+	dg.AddHandler(interactionCreate)
 
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("Error opening Discord session: ", err)
+		return
+	}
+
+	err = registerSlashCommand(dg, applicationID)
+	if err != nil {
+		fmt.Println("Error registering slash command: ", err)
+		dg.Close()
 		return
 	}
 
@@ -41,12 +50,69 @@ func main() {
 	dg.Close()
 }
 
+func registerSlashCommand(s *discordgo.Session, applicationID string) error {
+    command := &discordgo.ApplicationCommand{
+        Name:        "confessions",
+        Description: "Request confessions from the bot with an optional message.",
+        Options: []*discordgo.ApplicationCommandOption{
+            {
+                Type:        discordgo.ApplicationCommandOptionString,
+                Name:        "message",
+                Description: "Your confession message",
+                Required:    false,
+            },
+        },
+    }
+    
+    _, err := s.ApplicationCommandCreate(applicationID, "", command)
+    if err != nil {
+        return fmt.Errorf("error registering slash command: %v", err)
+    }
+    
+    fmt.Println("Slash command '/confessions' registered successfully.")
+    return nil
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID || m.Author.Bot {
 		return
 	}
 
 	if strings.HasPrefix(m.Content, "!hello") {
-		s.ChannelMessageSend(m.ChannelID, "Hello, I'm a Discord bot!")
+		s.ChannelMessageSend(m.ChannelID, "meow meow")
 	}
 }
+
+func interactionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+    if i.Type == discordgo.InteractionApplicationCommand {
+        switch i.ApplicationCommandData().Name {
+        case "confessions":
+            confessionMessage := ""
+            for _, opt := range i.ApplicationCommandData().Options {
+                if opt.Name == "message" && opt.Type == discordgo.ApplicationCommandOptionString {
+                    confessionMessage = opt.StringValue()
+                    break
+                }
+            }
+            
+            channelID := os.Getenv("ChannelID")
+            embed := &discordgo.MessageEmbed{
+                Type:        discordgo.EmbedTypeRich,
+                Title:       "Confession",
+                Description: confessionMessage,
+                Color:       0xff0000, 
+            }
+            
+            if confessionMessage != "" {
+                _, err := s.ChannelMessageSendEmbed(channelID, embed)
+                if err != nil {
+                    fmt.Println("Error sending embed message: ", err)
+                }
+            }
+        }
+    }
+}
+
+
+
+ 
